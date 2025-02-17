a754461a982d6b1cc8b2855ed6e8a561f4d5980f
Kyle Rowan Whitfield <146214009+shNNza@users.noreply.github.com>
2025-02-17 21:40:52 +0200

    Kyle Monday 015

--- player.py
+++ player.py
@@ -13,6 +13,8 @@ class Player(CircleShape):
         self.shots = shots
         self.rotation = 0
         self.direction = pygame.Vector2(0, -1)
+        self.PLAYER_SHOOT_COOLDOWN = 0.3
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -39,16 +41,28 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+        if self.shoot_timer < 0:
+            self.shoot_timer = 0
         
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot_velocity = self.direction * PLAYER_SHOOT_SPEED # Create a new shot at the player's position
-        new_shot = Shot(self.position, shot_velocity)
-        self.shots.add(new_shot)
+        if self.shoot_timer == 0:
+            self.shoot_timer = self.PLAYER_SHOOT_COOLDOWN
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            spawn_position = self.position + (forward * self.radius)
+            shot_velocity = forward * PLAYER_SHOOT_SPEED # Create a new shot at the player's position
+            new_shot = Shot(spawn_position, shot_velocity)
+            self.shots.add(new_shot)
 
     def handle_input(self, pressed_keys):
         if pressed_keys[pygame.K_SPACE]:
-            self.shoot()  
+            self.shoot()
+        if pressed_keys[pygame.K_LEFT]:
+            self.rotate(-1)
+        if pressed_keys[pygame.K_RIGHT]:
+            self.rotate(1)    
